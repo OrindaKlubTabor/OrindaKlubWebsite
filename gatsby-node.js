37d39def450aab251b2640c84d3af353dfcf9792
Erik Stoklasa <45731908+erikstoklasa@users.noreply.github.com>
2021-01-31 23:50:04 +0100

    Added pagination

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,8 @@
+const path = require("path")
+const { createFilePath } = require("gatsby-source-filesystem")
+
 exports.createPages = async ({ actions, graphql, reporter }) => {
+  const { createPage } = actions
   const result = await graphql(`
     {
       allContentfulPost {
@@ -10,7 +14,25 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
   `)
   if (result.errors) {
     reporter.panic("Error loading posts", JSON.stringify(result.errors))
+    return
   }
+
+  const posts = result.data.allContentfulPost.nodes
+  const postsPerPage = 6
+  const numPages = Math.ceil(posts.length / postsPerPage)
+  Array.from({ length: numPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/blog` : `/blog/${i + 1}`,
+      component: path.resolve("./src/templates/posts.js"),
+      context: {
+        limit: postsPerPage,
+        skip: i * postsPerPage,
+        numPages,
+        currentPage: i + 1,
+      },
+    })
+  })
+
   result.data.allContentfulPost.nodes.forEach(post => {
     actions.createPage({
       path: `/${post.slug}/`,
---
+++ src/templates/posts.js
@@ -0,0 +1,101 @@
+import React from "react"
+import Layout from "../components/layout"
+import Container from "../components/container"
+import { graphql, Link } from "gatsby"
+
+export default class BlogList extends React.Component {
+  render() {
+    const posts = this.props.data.allContentfulPost.nodes
+    const { currentPage, numPages } = this.props.pageContext
+    const isFirst = currentPage === 1
+    const isLast = currentPage === numPages
+    const prevPage =
+      currentPage - 1 === 1 ? "/blog" : "/blog/" + (currentPage - 1).toString()
+    const nextPage = "/blog/" + (currentPage + 1).toString()
+    const startCountIndex = numPages > 5 ? currentPage - 2 - 1 - isLast : 0
+    const endCountIndex = numPages > 5 ? currentPage + 2 + isFirst : numPages
+    return (
+      <Layout>
+        <Container>
+          <h1>Všechny příspěvky</h1>
+          {posts.map(node => (
+            <div style={{ margin: "3rem 0" }} key={node.slug}>
+              <h2 style={{ margin: "1rem 0" }}>{node.title}</h2>
+              <span>{node.createdAt}</span>
+              <br></br>
+              <span>{node.description}</span>
+              <br></br>
+              <br></br>
+              <Link className="link-important" to={`/${node.slug}`}>
+                Zobrazit příspěvek &#8594;
+              </Link>
+            </div>
+          ))}
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            {isFirst ? (
+              <span style={{ color: "transparent" }}>← Předchozí</span>
+            ) : (
+              <Link to={prevPage} rel="prev">
+                ← Předchozí
+              </Link>
+            )}
+            <div>
+              {Array.from(
+                { length: numPages },
+                (_, i) =>
+                  startCountIndex <= i &&
+                  endCountIndex > i &&
+                  (currentPage === i + 1 ? (
+                    <Link
+                      style={{
+                        padding: "0 10px",
+                        backgroundColor: "#002868",
+                        color: "#fff",
+                        borderRadius: 5,
+                      }}
+                      key={`pagination-number${i + 1}`}
+                      to={`${i === 0 ? "/blog" : "/blog/" + (i + 1)}`}
+                    >
+                      {i + 1}
+                    </Link>
+                  ) : (
+                    <Link
+                      style={{ padding: "0 10px" }}
+                      key={`pagination-number${i + 1}`}
+                      to={`${i === 0 ? "/blog" : "/blog/" + (i + 1)}`}
+                    >
+                      {i + 1}
+                    </Link>
+                  ))
+              )}
+            </div>
+            {isLast ? (
+              <span style={{ color: "transparent" }}>Další →</span>
+            ) : (
+              <Link to={nextPage} rel="next">
+                Další →
+              </Link>
+            )}
+          </div>
+        </Container>
+      </Layout>
+    )
+  }
+}
+
+export const data = graphql`
+  query blogListQuery($skip: Int!, $limit: Int!) {
+    allContentfulPost(
+      limit: $limit
+      skip: $skip
+      sort: { fields: createdAt, order: DESC }
+    ) {
+      nodes {
+        description
+        createdAt(formatString: "DD. MMMM YYYY", locale: "cs")
+        slug
+        title
+      }
+    }
+  }
+`
