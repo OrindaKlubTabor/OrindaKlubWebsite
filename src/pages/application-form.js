f433191132ee5dbd5a4578208d69e172f3837114
Erik Stoklasa <me@erikstoklasa.cz>
2022-08-17 11:29:03 +0200

    Added link to newsletter signup

--- src/pages/application-form.js
+++ src/pages/application-form.js
@@ -2,13 +2,33 @@ import React from "react"
 import Header from "../components/header"
 import Layout from "../components/layout"
 import Container from "../components/container"
+import { Link } from "gatsby"
 
 export default function application() {
   return (
     <Layout>
       <Container>
         <Header headerText="Soutěž 2021 je ukončená"></Header>
-        <p>Nemusíš se ale strachovat, na podzim 2022 spouštíme soutěž znovu. Zatím si můžeš přečíst, co budeš k přihlášce potřebovat. Určitě si také přečti některý z předcházejících příspěkvů našich studentů z Kalifornie.</p>
+        <p>
+          Nemusíš se ale strachovat, na podzim 2022 spouštíme soutěž znovu.
+          Zatím si můžeš přečíst, co budeš k přihlášce potřebovat. Určitě si
+          také přečti některý z předcházejících příspěkvů našich studentů z
+          Kalifornie.
+        </p>
+        <p>Zadej svůj email, aby Ti neunikl další ročník soutěže!</p>
+        <Link
+          style={{
+            background: "#002868",
+            color: "white",
+            padding: "1rem",
+            margin: "1rem 0 0",
+            display: "block",
+            maxWidth: "20rem",
+          }}
+          to="https://forms.gle/ey149oGRc99eiSBb8"
+        >
+          Odebírat novinky &#8594;
+        </Link>
         <h2>Obsah přihlášky</h2>
         <p>
           Motivační esej v anglickém jazyce v rozsahu přibližně 2 normostrany by
