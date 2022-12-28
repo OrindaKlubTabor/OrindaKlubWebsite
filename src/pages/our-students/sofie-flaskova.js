c0c0ccc9d32223cf90a13e59144ca26cbafed8ef
Erik Stoklasa <me@erikstoklasa.cz>
2022-12-28 21:58:39 +0100

    Updated student profile images loading

--- src/pages/our-students/sofie-flaskova.js
+++ src/pages/our-students/sofie-flaskova.js
@@ -2,42 +2,57 @@ import React from "react"
 import Layout from "../../components/layout"
 import Container from "../../components/container"
 import ProfilePic from "../../images/students/sofie-flaskova.png"
-import { Link } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 import { Helmet } from "react-helmet"
+import Img from "gatsby-image"
 
-export default () => (
-  <Layout>
-    <Helmet>
-      <title>Sofie Flašková - Orinda Klub Tábor</title>
-    </Helmet>
-    <Container>
-      <Link className="link-important" to="/our-students/">
-        &#8592; zpět na všechny studenty
-      </Link>
-      <div className="profile">
-        <img src={ProfilePic} alt="Sofie Flašková" />
-        <h1>Sofie Flašková</h1>
-        <small>Medailonek 2022/23</small>
-        <br></br>
-        <a
-          className="link-important"
-          href="https://www.instagram.com/sofieflaskovaa/"
-        >
-          Odkaz na Instagram →
-        </a>
-        <br></br>
-        <a
-          className="link-important"
-          href="https://www.youtube.com/channel/UCxXUMHrP8OBpI-FSj27GAqw"
-        >
-          Odkaz na YouTube kanál →
-        </a>
-        <p>
-          Sofie je momentálně v Orindě a vrací se v lednu 2023. Její
-          dobrodružství můžete sledovat na Instagramu nebo na YouTube, kam
-          přidává své dojmy z Kalifornie.
-        </p>
-      </div>
-    </Container>
-  </Layout>
-)
+export default () => {
+  const data = useStaticQuery(graphql`
+    query SofieQ {
+      file(relativePath: { eq: "students/sofie-flaskova.png" }) {
+        childImageSharp {
+          fixed(width: 200, quality: 90) {
+            originalName
+            ...GatsbyImageSharpFixed_withWebp_noBase64
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <Layout>
+      <Helmet>
+        <title>Sofie Flašková - Orinda Klub Tábor</title>
+      </Helmet>
+      <Container>
+        <Link className="link-important" to="/our-students/">
+          &#8592; zpět na všechny studenty
+        </Link>
+        <div className="profile">
+          <Img fixed={data.file.childImageSharp.fixed} alt="Sofie Flašková" />
+          <h1>Sofie Flašková</h1>
+          <small>Medailonek 2022/23</small>
+          <br></br>
+          <a
+            className="link-important"
+            href="https://www.instagram.com/sofieflaskovaa/"
+          >
+            Odkaz na Instagram →
+          </a>
+          <br></br>
+          <a
+            className="link-important"
+            href="https://www.youtube.com/channel/UCxXUMHrP8OBpI-FSj27GAqw"
+          >
+            Odkaz na YouTube kanál →
+          </a>
+          <p>
+            Sofie je momentálně v Orindě a vrací se v lednu 2023. Její
+            dobrodružství můžete sledovat na Instagramu nebo na YouTube, kam
+            přidává své dojmy z Kalifornie.
+          </p>
+        </div>
+      </Container>
+    </Layout>
+  )
+}
